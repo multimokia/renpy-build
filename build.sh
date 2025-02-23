fe91d70048cd334279e9d3b1429310dcb207c7d8
multimokia <misty@multimokia.studio>
2025-02-23 15:18:05 -0500

    better system

--- build.sh
+++ build.sh
@@ -12,16 +12,19 @@ apt-get update
 apt-get install -y git sudo
 
 # Now begin workflow
-git clone https://github.com/multimokia/renpy-build
+if [ ! -d "renpy-build" ]; then
+  git clone https://github.com/multimokia/renpy-build
 
-# Move deps over
-mv android-ndk-r27c-linux.zip renpy-build/tars/
-mv CubismSdkForNative-4-r.6.2.zip renpy-build/tars/
+  # Move deps over
+  mv android-ndk-r27c-linux.zip renpy-build/tars/
+  mv CubismSdkForNative-4-r.6.2.zip renpy-build/tars/
+fi 
 
 cd renpy-build
 
 # Temp until this goes into the master branch of this repo
 git checkout docker-support
+git pull
 
 chmod +x ./prepare.sh
 ./prepare.sh
